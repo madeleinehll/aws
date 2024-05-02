ed8618b62b12e6c412bad3f25a3a0ae7602440b4
madeleinehll <hill.madeleine13@yahoo.de>
2024-05-02 17:07:41 +0200

    ranges eintragen

---
+++ colors.js
@@ -0,0 +1,16 @@
+const COLORS = {
+temperature: [
+    {min: -50, max:-25, color: "#9f80ff"},
+    {min: -25, max:-20, color: "#784cff"},
+    {min: -20, max:-15, color: "#784cff"},
+    {min: -15, max:-10, color: "#784cff"},
+    {min: -5, max:0, color: "#784cff"},
+    {min: 0, max:5, color: "#784cff"},
+    {min: 5, max:10, color: "#784cff"},
+    {min: 10, max:15, color: "#784cff"},
+    {min: 15, max:20, color: "#784cff"},
+    {min: 20, max:25, color: "#784cff"},
+    {min: 25, max:30, color: "#784cff"},
+    {min: 30, max:50, color: "#784cff"},
+]
+};
